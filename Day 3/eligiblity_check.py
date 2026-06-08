57419e7f54bc60b2a2d6e0d0a4ec5f46334dd907
akshavhora513-png <akshavhora513@gmail.com>
2026-06-08 03:50:44 -0700

    first commit

---
+++ Day 3/eligiblity_check.py
@@ -0,0 +1,9 @@
+name = input("enter your name:")
+age =int(input("enter your age:"))
+pass_id = input("do you have pass(yes/no):")
+
+if age <= 20 and pass_id == "yes":
+     print("your eligiblity");
+else:
+     print("your not eligiblity");
+
---
+++ Day1/task5_pattern.py
@@ -0,0 +1,3 @@
+print("aksha")
+print("*****")
+print("*****")
